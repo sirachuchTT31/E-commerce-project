417b1188554322b22b9e7e49801ebec9b6d71ad5
sirachuch <sirachuch.kotuta@gmail.com>
2023-10-15 16:55:58 +0700

    [/] header set menu

--- src/app/component/header-global/header-global.component.ts
+++ src/app/component/header-global/header-global.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TokenStorageService } from 'src/app/shared/service/token.storage.service';
 
 @Component({
   selector: 'app-header-global',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./header-global.component.scss']
 })
 export class HeaderGlobalComponent {
+  is_Token: any
+  role: any
+  constructor(private localstorageService: TokenStorageService) {
 
+  }
+  ngOnInit() {
+    this.is_Token = this.localstorageService.tokenStorage
+    this.role = this.localstorageService.roleStorage
+  }
 }
